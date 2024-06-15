df7e3564adf87e49a093864aebf8972af9248357
LUCIFER49 <p1ush.prj@gmail.com>
2024-06-16 00:35:54 +0530

    Setting up protected route for auth

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 // User model
