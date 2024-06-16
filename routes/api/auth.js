1d7a67c5437f3ab53c8eca79e736ff839dd12095
LUCIFER49 <p1ush.prj@gmail.com>
2024-06-16 13:32:18 +0530

    Get User data for refernce

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -7,11 +7,13 @@ const User = require('../../models/User');
 // @desc        Test route
 // @access      Public
 router.get('/', auth, async (req, res) => {
-    try {
-        const user = await User.findById(req.user.id)
-    } catch (err) {
-        
-    }
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Serve Error');
+  }
 });
 
 module.exports = router;
