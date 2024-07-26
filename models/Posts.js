a5417e6610a9ae133756c792325b523770eaf29b
LUCIFER49 <p1ush.prj@gmail.com>
2024-07-26 21:53:28 +0530

    Post Model Created

---
+++ models/Posts.js
@@ -0,0 +1,55 @@
+const mongoose = required('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users',
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+  },
+  avatar: {
+    type: String,
+  },
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users',
+      },
+    },
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Typs.ObjectId,
+        ref: 'users',
+      },
+      text: {
+        type: String,
+        required: true,
+      },
+      name: {
+        type: String,
+      },
+      avatar: {
+        type: String,
+      },
+      date: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = Posts = mongoose.model('post', PostSchema); 
