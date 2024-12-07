95863c5eab316583128a2e23672c1faf710ebf06
LUCIFER49 <pyush.prj@gmail.com>
2024-12-07 18:28:49 +0530

    Like, Unlike & other functionality have been added

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -16,9 +16,9 @@ const Posts = ({ getPosts, post: { posts, loading } }) => {
     <Fragment>
       <h1 className='large text-primary'>Posts</h1>
       <p className='lead'>
-        <i className='fas fa-user'> Welcome to the community!</i>
+        <i className='fas fa-user'> Welcome to the community</i>
       </p>
-      {/* Post FOrm*/}
+      {/* Post Form*/}
       <div className='posts'>
         {posts.map((post) => (
           <PostItem key={post._id} post={post} />
--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,12 +1,6 @@
-import { GET_POSTS, POST_ERROR } from '../action/types';
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST } from '../action/types';
 
-const initialState = {
-  posts: [],
-  post: null,
-  loading: true,
-  error: null,
-  error: {},
-};
+const initialState = { posts: [], post: null, loading: true, error: null,  error: {} };
 
 export default function (state = initialState, action) {
   const { type, payload } = action;
@@ -18,13 +12,27 @@ export default function (state = initialState, action) {
         posts: payload,
         loading: false,
       };
+    case DELETE_POST:
+      return {
+        ...state,
+        posts: state.posts.filter((post) => post._id !== payload),
+        loading: false,
+      };
     case POST_ERROR:
       return {
         ...state,
         error: payload,
         loading: false,
       };
-    default: 
+    case UPDATE_LIKES:
+      return {
+        ...state,
+        posts: state.posts.map((post) =>
+          post._id === payload.id ? { ...post, likes: payload.likes } : post
+        ),
+        loading: false,
+      };
+    default:
       return state;
   }
 }
